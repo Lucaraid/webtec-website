e565d6737e89986fd380ecd35776a86dc5b2ed84
Lucaraid <luca.wandfluh@gmail.com>
2018-05-08 18:52:06 +0200

    feat: add form and server response

--- index.php
+++ index.php
@@ -16,6 +16,8 @@
 
     <!-- CSS
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/skeleton.css">
     <link rel="stylesheet" href="css/custom.css">
@@ -24,10 +26,21 @@
     <!-- JS
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
     <script src="js/skeleton.js"></script>
     <script src="js/jquery.cookie.js"></script>
     <script src="js/canvas.js"></script>
     <script src="js/functions.js"></script>
+
+    <?php
+    if (isset($_POST['submit'])){ ?>
+        <script>
+            alert("Name: <?= $_POST["name"]?>, " +
+                  "Email: <?= $_POST["email"]?>, " +
+                  "Text: <?= $_POST["text"]?>");
+        </script>
+    <?php } ?>
 
     <!-- Favicon
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
@@ -47,8 +60,8 @@
             <ul class="navbar-list">
                 <li class="navbar-item"><a class="navbar-link" href="#styleswitcher">Styleswitcher</a></li>
                 <li class="navbar-item"><a class="navbar-link" href="#canvas_hslu">Canvas</a></li>
-                <li class="navbar-item"><a class="navbar-link" href="#form">Kontaktformular</a></li>
                 <li class="navbar-item"><a class="navbar-link" href="#diashow">Diashow</a></li>
+                <li class="navbar-item"><a class="navbar-link" href="#form">Kontaktformular</a></li>
                 <li class="navbar-item"><a class="navbar-link" href="#pictures">Bilder</a></li>
             </ul>
         </div>
@@ -76,6 +89,62 @@
                 <canvas id="canvas">
                 </canvas>
             </div>
+        </div>
+    </div>
+    <div class="docs-section" id="diashow">
+        <div class="row">
+            <h4>Diashow</h4>
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                </ol>
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner">
+                    <div class="item active">
+                        <img src="images/1.png" alt="Los Angeles" style="width:100%;">
+                    </div>
+
+                    <div class="item">
+                        <img src="images/2.png" alt="Chicago" style="width:100%;">
+                    </div>
+
+                    <div class="item">
+                        <img src="images/3.png" alt="New york" style="width:100%;">
+                    </div>
+                </div>
+
+                <!-- Left and right controls -->
+                <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="docs-section" id="form">
+        <div class="row">
+            <h4>Form</h4>
+            <form id="innerform" action="index.php" method="post">
+                <div class="six columns">
+                    <label for="name">Your name</label>
+                    <input class="u-full-width" id="name" name="name" type="text" placeholder="John Diggle">
+                </div>
+                <div class="six columns">
+                    <label for="email">Your email</label>
+                    <input class="u-full-width" id="email" name="email" type="email" placeholder="example@gmail.com">
+                </div>
+                <label for="text">Message</label>
+                <textarea class="u-full-width" id="text" name="text" placeholder="Hello, ..."></textarea>
+                <input class="button-primary" name="submit" type="submit" value="Submit">
+            </form>
         </div>
     </div>
 </div>
